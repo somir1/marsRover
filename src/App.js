a4d0ddbf2feae16e7447eafa04b82a8a528f6d11
somir1 <leonsomir@gmail.com>
2022-01-03 16:40:17 -0600

    marsrover

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Search from './components/Search';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="App">
+          <div className = "test">
+            <h2>Welcome</h2>
+            <p>Please input a date below if no images show please select any date after the year 2013 </p>
+            <Search></Search>
+          </div>
+        </div>
   );
 }
 
